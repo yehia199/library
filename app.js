94f698dec404158e405913cc8be8d1e1268cf688
yehia199 <yehiacomputer08@gmail.com>
2023-08-10 12:55:54 +0300

    i made the constructor object

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ function setTheme() {
 }
 let themeButton = document.querySelector('.theme-switcher');
 themeButton.addEventListener('click', setTheme);
+
+const addButton = document.querySelector('.add-items')
+const removeButton = document.querySelector('.remove-all')
+const booksContainer = document.querySelector('#cards-container')
+
+let library = [];1
+
+function Book(title, author, pages, isRead) {
+   this.title = title;
+   this.author = author;
+   this.pages = pages;
+   this.isRead = isRead;
+ }
+
+ function addToLibrary() {
+  addButton.addEventListener('click', () => {
+    library.push(new Book(prompt(''),prompt(''),prompt(''),prompt('true of false')))
+   })
+ }
+addToLibrary()
+console.log(library)
