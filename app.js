85445a029b6460322847ed32fc15d230094fe764
yehia199 <yehiacomputer08@gmail.com>
2023-08-13 13:53:13 +0300

    i made a dialog that contains the form

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ function setTheme() {
   let newTheme = root.className === 'dark' ? 'light' : 'dark';
   root.className = newTheme;
   let themeName = document.querySelector('.theme-name');
-  let themeNameChanger = newTheme === 'dark' ? themeName.textContent = 'light' : themeName.textContent = 'dark'; 
-  themeName.textContent = themeNameChanger;
+  themeName.textContent = newTheme;
 }
 let themeButton = document.querySelector('.theme-switcher');
 themeButton.addEventListener('click', setTheme);
@@ -14,8 +13,8 @@ themeButton.addEventListener('click', setTheme);
 const addButton = document.querySelector('.add-items')
 const removeButton = document.querySelector('.remove-all')
 const booksContainer = document.querySelector('#cards-container')
-
-let library = [];1
+const dialoge = document.querySelector('#form-dialog')
+let library = [];
 
 function Book(title, author, pages, isRead) {
    this.title = title;
@@ -24,10 +23,9 @@ function Book(title, author, pages, isRead) {
    this.isRead = isRead;
  }
 
- function addToLibrary() {
-  addButton.addEventListener('click', () => {
-    library.push(new Book(prompt(''),prompt(''),prompt(''),prompt('true of false')))
-   })
- }
+ addButton.addEventListener('click', ()=>{
+   dialoge.showModal()
+ })
+ 
 addToLibrary()
 console.log(library)
